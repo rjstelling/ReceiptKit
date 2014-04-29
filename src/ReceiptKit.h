3d6b2346f23aaa2a963f55e17620d106acaad25b
Richard Stelling <richard@empiricalmagic.com>
2014-04-29 19:25:05 +0100

    - Added ReceiptKit.h for easy #import

---
+++ src/ReceiptKit.h
@@ -0,0 +1,11 @@
+//
+//  ReceiptKit.h
+//  ReceiptKit
+//
+//  Created by Richard Stelling on 29/04/2014.
+//  Copyright (c) 2014 Empirical Magic Ltd. All rights reserved.
+//
+
+#import "RTKReceiptParser.h"
+#import "RTKPurchaseInformation.h"
+#import "RTKInAppPurchaseInformation.h"
